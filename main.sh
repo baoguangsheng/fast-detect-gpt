277924ee65415917884ce1d0504983f9bb38f6e3
Bao Guangsheng <gshbao@hotmail.com>
2023-11-28 08:26:17 +0800

    Update main.sh
    add DNA-GPT as a baseline

--- main.sh
+++ main.sh
@@ -41,6 +41,15 @@ for D in $datasets; do
   done
 done
 
+# evaluate DNA-GPT
+for D in $datasets; do
+  for M in $source_models; do
+    echo `date`, Evaluating baseline methods on ${D}_${M} ...
+    python scripts/dna_gpt.py --base_model_name $M --dataset $D \
+                          --dataset_file $data_path/${D}_${M} --output_file $res_path/${D}_${M}
+  done
+done
+
 # evaluate DetectGPT and its improvement DetectLLM
 for D in $datasets; do
   for M in $source_models; do
